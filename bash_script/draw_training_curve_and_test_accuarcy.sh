421e77349287387454bb78c0f9627f94fd15f44b
Lingcao Huang <huanglingcao@gmail.com>
2017-05-21 20:25:24 +0800

    update draw script

--- bash_script/draw_training_curve_and_test_accuarcy.sh
+++ bash_script/draw_training_curve_and_test_accuarcy.sh
@@ -10,7 +10,7 @@ echo saving png file is: $savepng
 #infoLog=/Users/huanglingcao/codes/PycharmProjects/rsBuildingSeg/caffe.bin.Cryo06.hlc.log.INFO.20170419-181307.6896
 
 home_dir=($HOME)
-codes_dir=$home_dir/codes/PycharmProjects/rsBuildingSeg
+codes_dir=$home_dir/codes/rsBuildingSeg
 draw_script=($codes_dir/DeepLab-Context/tools/extra/plot_training_log.py.example)
 
 echo draw_script: ${draw_script}
