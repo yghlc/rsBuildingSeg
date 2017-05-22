2666b9f0d6d1f98ac5487f11b54a12b3229ae7ad
Lingcao Huang <huanglingcao@gmail.com>
2017-05-22 21:12:45 +0800

    HECK_EQ(target_blobs[j]->channels(), source_layer.blobs(j).channels())

--- DeepLab-Context/src/caffe/net.cpp
+++ DeepLab-Context/src/caffe/net.cpp
@@ -714,7 +714,7 @@ void Net<Dtype>::CopyTrainedLayersFrom(const NetParameter& param) {
       if (strict_dim) {
 	CHECK_EQ(target_blobs[j]->num(), source_layer.blobs(j).num())
 	  << "Incompatible parameter size for layer " << source_layer_name;
-//	CHECK_EQ(target_blobs[j]->channels(), source_layer.blobs(j).channels())
+//	CHECK_EQ(target_blobs[j]->channels(), source_layer.blobs(j).channels())     //lingcao
 //	  << "Incompatible parameter size for layer " << source_layer_name;
 	CHECK_EQ(target_blobs[j]->height(), source_layer.blobs(j).height())
 	  << "Incompatible parameter size for layer " << source_layer_name;
